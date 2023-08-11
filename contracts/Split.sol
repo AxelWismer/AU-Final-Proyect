f8672f6e50c54bb2ad10ef7be08ff971ddd4268d
AxelWismer <mmowismer@outlook.com>
2023-08-11 16:50:10 +0200

    added test hierarchy

--- contracts/Split.sol
+++ contracts/Split.sol
@@ -2,142 +2,132 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract Split {
-    /**
-     * Contract Data
-     **/
-    // Balances
-    mapping(address => uint256) public balances;
+  /**
+   * Contract Data
+   **/
+  // Balances
+  mapping(address => uint256) public balances;
 
-    // Groups
-    struct Group {
-        address creator;
+  // Groups
+  struct Group {
+    address creator;
+  }
+
+  uint128 public groupCount;
+  mapping(uint128 => Group) public groups;
+  mapping(uint128 => mapping(address => bool)) public groupMembers;
+
+  // Expenses
+  enum Status {
+    CREATED,
+    DISPUTED,
+    CANCELED,
+    PAYED
+  }
+
+  // TODO: Reeplace members version and member history with a list of debtors
+  // TODO: Remove history of members and replace it with a mapping mto manage access and array to keep track of all the addresses
+
+  struct Expense {
+    Status status; //uint8
+    address creditor;
+    uint32 yesCount;
+    uint32 noCount;
+    address[] debtors;
+  }
+
+  // Maps: groupID => expenseID => memberId => vote
+  mapping(uint128 => mapping(uint128 => mapping(address => bool))) votes;
+  // Maps: groupID => expenseID => memberId => debt
+  mapping(uint128 => mapping(uint128 => mapping(address => uint256))) debts;
+
+  /**
+   * Events
+   **/
+  event GroupCreated(uint128 groupID, address creator, string[3] info);
+
+  event ExpenseCreated(
+    uint128 groupId,
+    uint128 expensesId,
+    uint256 amount,
+    address creator,
+    string[3] info,
+    address[] debtors,
+    uint256[] debts
+  );
+
+  mapping(uint128 => Expense[]) groupExpenses;
+
+  /**
+   * Modifiers
+   **/
+  modifier onlyMember(uint128 _groupId) {
+    require(isMember(_groupId, msg.sender), "You are not a member of the group");
+    _;
+  }
+
+  /**
+   * Functions
+   **/
+
+  constructor() {}
+
+  // Utilities
+  function isMember(uint128 _groupId, address member) public view returns (bool) {
+    return groupMembers[_groupId][member];
+  }
+
+  /**
+   * Change state
+   **/
+  // Groups
+  function createGroup(address[] calldata _groupMembers, string[3] calldata _info) external {
+    // Groups Id range from 1 to group count
+    groupCount++;
+    groups[groupCount] = Group(msg.sender);
+    for (uint32 memberId = 1; memberId <= _groupMembers.length; memberId++) {
+      groupMembers[groupCount][_groupMembers[memberId - 1]] = true;
     }
+    emit GroupCreated(groupCount, msg.sender, _info);
+  }
 
-    uint128 public groupCount;
-    mapping(uint128 => Group) public groups;
-    mapping(uint128 => mapping(address => bool)) public groupMembers;
-
-    // Expenses
-    enum Status {
-        CREATED,
-        DISPUTED,
-        CANCELED,
-        PAYED
+  //Expenses
+  function addExpense(
+    uint128 _groupId,
+    address[] calldata _debtors,
+    uint256[] calldata _debts,
+    string[3] calldata _info
+  ) external onlyMember(_groupId) {
+    // Check _debtors and _debts format
+    require(_debtors.length == _debts.length, "The arrays must have the same size");
+    for (uint256 pos = 0; pos < _debtors.length; pos++) {
+      // Check that the sender is not one of the debtors
+      require(_debtors[pos] != msg.sender, "You can't be a debtor in one of your expenses");
+      // Check that all debtors are members in the group
+      require(isMember(_groupId, _debtors[pos]), "All the debtors must be members of the group");
+      // Check that all the debts are greater than 0
+      require(_debts[pos] > 0);
     }
-
-    // TODO: Reeplace members version and member history with a list of debtors
-    // TODO: Remove history of members and replace it with a mapping mto manage access and array to keep track of all the addresses
-
-    struct Expense {
-        Status status; //uint8
-        address creditor;
-        uint32 yesCount;
-        uint32 noCount;
-        address[] debtors;
-    }
-
-    // Maps: groupID => expenseID => memberId => vote
-    mapping(uint128 =>  mapping (uint128 => mapping(address => bool))) votes;
-    // Maps: groupID => expenseID => memberId => debt
-    mapping(uint128 =>  mapping (uint128 => mapping(address => uint256))) debts;
-
-    /**
-     * Events
-     **/
-    event GroupCreated(uint128 groupID, address creator, string[3] info);
-
-    event ExpenseCreated(
-        uint128 groupId,
-        uint128 expensesId,
-        uint256 amount,
-        address creator,
-        string[3] info,
-        address[] debtors,
-        uint[] debts
+    // Add the expense to the expense list
+    groupExpenses[_groupId].push(
+      Expense({
+        status: Status.CREATED, //uint8
+        creditor: msg.sender,
+        yesCount: 0,
+        noCount: 0,
+        debtors: _debtors
+      })
     );
-
-    mapping(uint128 => Expense[]) groupExpenses;
-
-    /**
-     * Modifiers
-     **/
-    modifier onlyMember(uint128 _groupId) {
-        require(
-            isMember(_groupId, msg.sender),
-            "You are not a member of the group"
-        );
-        _;
+    // Add the debt for each member
+    uint128 expensesId = uint128(groupExpenses[_groupId].length);
+    uint256 _amount = 0;
+    for (uint256 pos = 0; pos < _debtors.length; pos++) {
+      uint256 debt = _debts[pos];
+      _amount += debt;
+      debts[_groupId][expensesId][_debtors[pos]] = debt;
     }
 
-    /**
-     * Functions
-     **/
-
-    constructor() {}
-
-    // Utilities
-    function isMember(uint128 _groupId, address member) public view returns (bool) {
-        return groupMembers[_groupId][member];
-    }
-
-    /**
-     * Change state
-     **/
-    // Groups
-    function createGroup(
-        address[] calldata _groupMembers,
-        string[3] calldata _info
-    ) external {
-        // Groups Id range from 1 to group count
-        groupCount++;
-        groups[groupCount] = Group(msg.sender);
-        for (
-            uint32 memberId = 1;
-            memberId <= _groupMembers.length;
-            memberId++
-        ) {
-            groupMembers[groupCount][_groupMembers[memberId - 1]] = true;
-        }
-        emit GroupCreated(groupCount, msg.sender, _info);
-    }
-
-    //Expenses
-    function addExpense(
-        uint128 _groupId,
-        address[] calldata _debtors,
-        uint256[] calldata _debts,
-        string[3] calldata _info
-    ) external onlyMember(_groupId) {
-        // Check _debtors and _debts format
-        require(_debtors.length == _debts.length, "The arrays must have the same size");
-        for (uint256 pos = 0; pos < _debtors.length; pos++) {
-            // Check that the sender is not one of the debtors
-            require(_debtors[pos] != msg.sender, "You can't be a debtor in one of your expenses");
-            // Check that all debtors are members in the group
-            require(isMember(_groupId, _debtors[pos]), "All the debtors must be members of the group");
-            // Check that all the debts are greater than 0 
-            require(_debts[pos] > 0);
-        }
-        // Add the expense to the expense list
-        groupExpenses[_groupId].push(
-            Expense({
-                status: Status.CREATED, //uint8
-                creditor: msg.sender,
-                yesCount: 0,
-                noCount: 0,
-                debtors: _debtors
-            })
-        );
-        // Add the debt for each member
-        uint128 expensesId = uint128(groupExpenses[_groupId].length);
-        uint256 _amount = 0;
-        for (uint256 pos = 0; pos < _debtors.length; pos++) {
-            uint256 debt = _debts[pos];
-            _amount += debt;
-            debts[_groupId][expensesId][_debtors[pos]] = debt;
-        }
-
-        // Create a new event
-        emit ExpenseCreated(_groupId, expensesId, _amount, msg.sender, _info, _debtors, _debts);
-    }
+    // Create a new event
+    emit ExpenseCreated(_groupId, expensesId, _amount, msg.sender, _info, _debtors, _debts);
+  }
 }
--- tests/split.test.js
+++ tests/split.test.js
@@ -1,90 +1,118 @@
 // Right click on the script name and hit "Run" to execute
-const {expect} = require('chai')
-const {ethers} = require('hardhat')
+const {expect} = require("chai")
+const {ethers} = require("hardhat")
 
-describe('Split', function () {
-    let Split
-    let split
-    let signers
-    beforeEach(async function () {
-        Split = await ethers.getContractFactory('Split')
-        split = await Split.deploy()
-        await split.deployed()
-        signers = await ethers.getSigners()
+describe("Split", function () {
+  let Split
+  let split
+  let signers
+
+  beforeEach(async function () {
+    Split = await ethers.getContractFactory("Split")
+    split = await Split.deploy()
+    await split.deployed()
+    signers = await ethers.getSigners()
+  })
+  it("should create the contract", async function () {
+    console.log("split deployed at:" + split.address)
+  })
+
+  describe("createGroup()", function () {
+    async function data() {
+      const [owner, addr1, addr2] = signers
+      return {
+        members1: [owner.address, addr2.address],
+        members2: [owner.address, addr1.address],
+        info1: ["First Group", "A description", "img1"],
+        info2: ["Second Group", "A description", "img2"],
+        creator1: owner,
+        creator1addr: owner.address,
+        creator2: addr1,
+        creator2addr: addr1.address,
+      }
+    }
+    async function createGroups() {
+      const {members1, members2, info1, info2, creator2} = await data()
+      await split.createGroup(members1, info1)
+      await split.connect(creator2).createGroup(members2, info2)
+    }
+    it("should emit GroupCreated event", async function () {
+      const {members1, members2, info1, info2, creator2, creator1addr, creator2addr} = await data()
+      // Create Groups
+      await expect(split.createGroup(members1, info1))
+        .to.emit(split, "GroupCreated")
+        .withArgs(1, creator1addr, info1)
+      await expect(split.connect(creator2).createGroup(members2, info2))
+        .to.emit(split, "GroupCreated")
+        .withArgs(2, creator2addr, info2)
     })
-    it('should create the contract', async function () {
-        console.log('split deployed at:' + split.address)
+    it("should increase the groupcount", async function () {
+      expect(await split.groupCount()).to.equal(0)
+      await createGroups()
+      expect(await split.groupCount()).to.equal(2)
     })
-    it('should create a group', async function () {
-        const [owner, addr1, addr2] = signers
-
-        const members1 = [owner.address, addr2.address]
-        const members2 = [owner.address, addr1.address]
-        const info1 = ['First Group', 'A description', 'img1']
-        const info2 = ['Second Group', 'A description', 'img2']
-        const creator1 = owner
-        const creator1addr = owner.address
-        const creator2 = addr1
-        const creator2addr = addr1.address
-
-        it('should emit GroupCreated event', async function () {
-            // Create Groups
-            await expect(split.createGroup(members1, info1))
-                .to.emit(split, 'GroupCreated')
-                .withArgs(1, creator1addr, info1)
-            await expect(split.connect(creator2).createGroup(members2, info2))
-                .to.emit(split, 'GroupCreated')
-                .withArgs(2, creator2addr, info2)
-        })
-
-        // // Check group count
-        // expect(await split.groupCount()).to.equal(2)
-        // // Check Creators
-        // expect(await split.groups(1)).to.equal(creator1addr)
-        // expect(await split.groups(2)).to.equal(creator2addr)
-        // // Check Members
-        // expect(await split.isMember(1, owner.address)).to.equal(true)
-        // expect(await split.isMember(1, addr1.address)).to.equal(false)
-        // expect(await split.isMember(1, addr2.address)).to.equal(true)
-        // expect(await split.isMember(2, owner.address)).to.equal(true)
-        // expect(await split.isMember(2, addr1.address)).to.equal(true)
-        // expect(await split.isMember(2, addr2.address)).to.equal(false)
-
+    it("should store the msg sender as the creator", async function () {
+      await createGroups()
+      const {creator1addr, creator2addr} = await data()
+      expect(await split.groups(1)).to.equal(creator1addr)
+      expect(await split.groups(2)).to.equal(creator2addr)
     })
-    it('should add new expenses', async function () {
-        const [owner, addr1, addr2] = signers
-
-        const members1 = [owner.address, addr1.address, addr2.address]
-        const debtors1 = [addr1.address, addr2.address]
-        const debtors2 = [owner.address]
-
-        const debts1 = [15, 7]
-        const debts2 = [4]
-
-        const info1 = ['First Expense', 'A description', 'img1']
-        const info2 = ['Second Expense', 'A description', 'img2']
-
-        const creator1 = owner
-        const creator1addr = owner.address
-        const creator2 = addr1
-        const creator2addr = addr1.address
-
-        // Create Groups
-        expect(await split.createGroup(members1, info1))
-            .to.emit(split, 'GroupCreated')
-            .withArgs(1, creator1addr, info1)
-
-        // Create expenses
-        await expect(split.addExpense(1, debtors1, debts1, info1))
-            .to.emit(split, 'ExpenseCreated')
-            .withArgs(1, 1, 22, creator1addr, info1, debtors1, debts1)
-        await expect(
-            split.connect(creator2).addExpense(1, debtors2, debts2, info2)
-        )
-            .to.emit(split, 'ExpenseCreated')
-            .withArgs(1, 2, 4, creator2addr, info2, debtors2, debts2)
-
-        // Get expenses members
-        // expect(await split.getExpenseMembers(1, 1)).to.eql(members1);
+    it("should store the members selected", async function () {
+      await createGroups()
+      const [owner, addr1, addr2] = signers
+      expect(await split.isMember(1, owner.address)).to.equal(true)
+      expect(await split.isMember(1, addr1.address)).to.equal(false)
+      expect(await split.isMember(1, addr2.address)).to.equal(true)
+      expect(await split.isMember(2, owner.address)).to.equal(true)
+      expect(await split.isMember(2, addr1.address)).to.equal(true)
+      expect(await split.isMember(2, addr2.address)).to.equal(false)
     })
+  })
+  describe("addExpense()", function () {
+    async function data() {
+      const [owner, addr1, addr2] = signers
+      return {
+        members1: [owner.address, addr1.address, addr2.address],
+        debtors1: [addr1.address, addr2.address],
+        debtors2: [owner.address],
+        debts1: [15, 7],
+        debts2: [4],
+        info1: ["First Expense", "A description", "img1"],
+        info2: ["Second Expense", "A description", "img2"],
+        creator1: owner,
+        creator1addr: owner.address,
+        creator2: addr1,
+        creator2addr: addr1.address,
+      }
+    }
+
+    async function addExpenses() {
+      await split.createGroup(members1, info1)
+      split.addExpense(1, debtors1, debts1, info1)
+      split.connect(creator2).addExpense(1, debtors2, debts2, info2)
+    }
+    it("should emit ExpenseCreated", async function () {
+      const {
+        members1,
+        debtors1,
+        debtors2,
+        debts1,
+        debts2,
+        info1,
+        info2,
+        creator2,
+        creator1addr,
+        creator2addr,
+      } = await data()
+
+      await split.createGroup(members1, info1)
+      await expect(split.addExpense(1, debtors1, debts1, info1))
+        .to.emit(split, "ExpenseCreated")
+        .withArgs(1, 1, 22, creator1addr, info1, debtors1, debts1)
+      await expect(split.connect(creator2).addExpense(1, debtors2, debts2, info2))
+        .to.emit(split, "ExpenseCreated")
+        .withArgs(1, 2, 4, creator2addr, info2, debtors2, debts2)
+    })
+    it("should add new expenses", async function () {})
+  })
 })
