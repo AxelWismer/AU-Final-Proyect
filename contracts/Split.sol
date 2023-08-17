92c6fb2fe1031b8d011a3eff18928bc8b2dd4cf6
AxelWismer <mmowismer@outlook.com>
2023-08-17 13:29:39 +0200

    added test for addExpense

--- contracts/Split.sol
+++ contracts/Split.sol
@@ -6,16 +6,16 @@ contract Split {
    * Contract Data
    **/
   // Balances
-  mapping(address => uint256) public balances;
+  mapping(address => uint) public balances;
 
   // Groups
   struct Group {
     address creator;
   }
 
-  uint128 public groupCount;
-  mapping(uint128 => Group) public groups;
-  mapping(uint128 => mapping(address => bool)) public groupMembers;
+  uint public groupCount;
+  mapping(uint => Group) public groups;
+  mapping(uint => mapping(address => bool)) public groupMembers;
 
   // Expenses
   enum Status {
@@ -33,35 +33,36 @@ contract Split {
     address creditor;
     uint32 yesCount;
     uint32 noCount;
-    address[] debtors;
   }
 
   // Maps: groupID => expenseID => memberId => vote
-  mapping(uint128 => mapping(uint128 => mapping(address => bool))) votes;
+  mapping(uint => mapping(uint => mapping(address => bool))) public votes;
   // Maps: groupID => expenseID => memberId => debt
-  mapping(uint128 => mapping(uint128 => mapping(address => uint256))) debts;
+  mapping(uint => mapping(uint => mapping(address => uint))) public debts;
+  // Maps: groupID => expenseID => memberId => debtor
+  mapping(uint => mapping(uint => address[])) public debtors;
 
+  mapping(uint => Expense[]) groupExpenses;
   /**
    * Events
    **/
-  event GroupCreated(uint128 groupID, address creator, string[3] info);
+  event GroupCreated(uint groupID, address creator, string[3] info);
 
   event ExpenseCreated(
-    uint128 groupId,
-    uint128 expensesId,
-    uint256 amount,
+    uint groupId,
+    uint expensesId,
+    uint amount,
     address creator,
     string[3] info,
     address[] debtors,
-    uint256[] debts
+    uint[] debts
   );
 
-  mapping(uint128 => Expense[]) groupExpenses;
 
   /**
    * Modifiers
    **/
-  modifier onlyMember(uint128 _groupId) {
+  modifier onlyMember(uint _groupId) {
     require(isMember(_groupId, msg.sender), "You are not a member of the group");
     _;
   }
@@ -72,11 +73,53 @@ contract Split {
 
   constructor() {}
 
-  // Utilities
-  function isMember(uint128 _groupId, address member) public view returns (bool) {
-    return groupMembers[_groupId][member];
+  /**
+   * View Functions
+   **/
+  function isMember(uint _groupId, address _member) public view returns (bool) {
+    return groupMembers[_groupId][_member];
   }
 
+  function getDebtors(uint _groupId, uint _expenseId) public view returns (address[] memory) {
+    return debtors[_groupId][_expenseId];
+  }
+
+  function getExpense(uint _groupId, uint _expenseId) public view returns (Expense memory) {
+    return groupExpenses[_groupId][_expenseId - 1];
+  }
+
+  function getExpenseCount(uint _groupId) public view returns (uint) {
+    return groupExpenses[_groupId].length;
+  }
+
+  function getExpenses(uint _groupId) public view returns (Expense[] memory) {
+    return groupExpenses[_groupId];
+  }
+
+  function getExpensesFrom(uint _groupId, uint _from) public view returns (Expense[] memory) {
+    return getExpensesFromTo(_groupId, _from, getExpenseCount(_groupId));
+  }
+
+  function getLastExpenses(uint _groupId, uint _last) public view returns (Expense[] memory) {
+    return getExpensesFromTo(_groupId, getExpenseCount(_groupId) - _last + 1 , getExpenseCount(_groupId));
+  }
+
+  function getExpensesFromTo(uint _groupId, uint _from, uint _to) public view returns (Expense[] memory) {
+    require(_from >= 1, "From parameter must be at least 1");
+    require(_from <= getExpenseCount(_groupId), "To parameter must be equal or less than the expese count");
+    require(_from <= _to, "From parameter must be equal or less than To parameter");
+    
+    return this.sliceExpenses(_from, _to, groupExpenses[_groupId]);
+  }
+
+  // This functions allows to slice a memory array
+  function sliceExpenses(uint _from, uint _to, Expense[] calldata expenses) public pure returns (Expense[] memory) {
+    return expenses[_from - 1: _to];
+  }
+
+
+
+  
   /**
    * Change state
    **/
@@ -93,20 +136,21 @@ contract Split {
 
   //Expenses
   function addExpense(
-    uint128 _groupId,
+    uint _groupId,
     address[] calldata _debtors,
-    uint256[] calldata _debts,
+    uint[] calldata _debts,
     string[3] calldata _info
   ) external onlyMember(_groupId) {
     // Check _debtors and _debts format
-    require(_debtors.length == _debts.length, "The arrays must have the same size");
-    for (uint256 pos = 0; pos < _debtors.length; pos++) {
+    require(_debtors.length == _debts.length, "The lists of debtors and debts must be of the same length");
+    require(_debtors.length > 0, "You must record at least 1 debt");
+    for (uint pos = 0; pos < _debtors.length; pos++) {
       // Check that the sender is not one of the debtors
       require(_debtors[pos] != msg.sender, "You can't be a debtor in one of your expenses");
       // Check that all debtors are members in the group
       require(isMember(_groupId, _debtors[pos]), "All the debtors must be members of the group");
       // Check that all the debts are greater than 0
-      require(_debts[pos] > 0);
+      require(_debts[pos] > 0, "The amount of the debts must be greater than 0");
     }
     // Add the expense to the expense list
     groupExpenses[_groupId].push(
@@ -114,20 +158,26 @@ contract Split {
         status: Status.CREATED, //uint8
         creditor: msg.sender,
         yesCount: 0,
-        noCount: 0,
-        debtors: _debtors
+        noCount: 0
       })
     );
+    uint expensesId = uint(groupExpenses[_groupId].length);
+    
     // Add the debt for each member
-    uint128 expensesId = uint128(groupExpenses[_groupId].length);
-    uint256 _amount = 0;
-    for (uint256 pos = 0; pos < _debtors.length; pos++) {
-      uint256 debt = _debts[pos];
+    uint _amount = 0;
+    for (uint pos = 0; pos < _debtors.length; pos++) {
+      uint debt = _debts[pos];
       _amount += debt;
       debts[_groupId][expensesId][_debtors[pos]] = debt;
     }
+    // Add debtors
+    debtors[_groupId][expensesId] = _debtors;
 
     // Create a new event
     emit ExpenseCreated(_groupId, expensesId, _amount, msg.sender, _info, _debtors, _debts);
   }
+
+  // Add pay method 
+  // Add retieve method
+  // Add dispute method (later)
 }
--- tests/split.test.js
+++ tests/split.test.js
@@ -1,6 +1,6 @@
 // Right click on the script name and hit "Run" to execute
-const {expect} = require("chai")
-const {ethers} = require("hardhat")
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 describe("Split", function () {
   let Split
@@ -32,12 +32,12 @@ describe("Split", function () {
       }
     }
     async function createGroups() {
-      const {members1, members2, info1, info2, creator2} = await data()
+      const { members1, members2, info1, info2, creator2 } = await data()
       await split.createGroup(members1, info1)
       await split.connect(creator2).createGroup(members2, info2)
     }
     it("should emit GroupCreated event", async function () {
-      const {members1, members2, info1, info2, creator2, creator1addr, creator2addr} = await data()
+      const { members1, members2, info1, info2, creator2, creator1addr, creator2addr } = await data()
       // Create Groups
       await expect(split.createGroup(members1, info1))
         .to.emit(split, "GroupCreated")
@@ -53,7 +53,7 @@ describe("Split", function () {
     })
     it("should store the msg sender as the creator", async function () {
       await createGroups()
-      const {creator1addr, creator2addr} = await data()
+      const { creator1addr, creator2addr } = await data()
       expect(await split.groups(1)).to.equal(creator1addr)
       expect(await split.groups(2)).to.equal(creator2addr)
     })
@@ -70,11 +70,12 @@ describe("Split", function () {
   })
   describe("addExpense()", function () {
     async function data() {
-      const [owner, addr1, addr2] = signers
+      const [owner, addr1, addr2, addr3] = signers
       return {
         members1: [owner.address, addr1.address, addr2.address],
         debtors1: [addr1.address, addr2.address],
         debtors2: [owner.address],
+        debtors3: [addr1.address, addr3.address],
         debts1: [15, 7],
         debts2: [4],
         info1: ["First Expense", "A description", "img1"],
@@ -87,10 +88,21 @@ describe("Split", function () {
     }
 
     async function addExpenses() {
+      const {
+        members1,
+        debtors1,
+        debtors2,
+        debts1,
+        debts2,
+        info1,
+        info2,
+        creator2,
+      } = await data()
       await split.createGroup(members1, info1)
-      split.addExpense(1, debtors1, debts1, info1)
-      split.connect(creator2).addExpense(1, debtors2, debts2, info2)
+      await split.addExpense(1, debtors1, debts1, info1)
+      await split.connect(creator2).addExpense(1, debtors2, debts2, info2)
     }
+
     it("should emit ExpenseCreated", async function () {
       const {
         members1,
@@ -113,6 +125,70 @@ describe("Split", function () {
         .to.emit(split, "ExpenseCreated")
         .withArgs(1, 2, 4, creator2addr, info2, debtors2, debts2)
     })
-    it("should add new expenses", async function () {})
+
+    it("should show the expense count for each group", async function () {
+      await addExpenses();
+      expect(await split.getExpenseCount(1)).to.equal(2); 
+      expect(await split.getExpenseCount(2)).to.equal(0);
+    })
+    it("should show the registred expenses", async function () {
+      const {
+        creator1addr,
+        creator2addr
+      } = await data()
+      await addExpenses();
+      expect(await split.getExpenseCount(1)).to.equal(2); 
+      expect(await split.getExpenseCount(2)).to.equal(0);
+      
+      const expenses = [[0,creator1addr,0,0], [0,creator2addr,0,0]];
+      expect(await split.getExpenses(1)).to.eql(expenses); 
+      expect(await split.getExpensesFromTo(1, 1, 1)).to.eql(expenses.slice(0, 1));
+      expect(await split.getExpensesFromTo(1, 2, 2)).to.eql(expenses.slice(1));
+      expect(await split.getExpensesFromTo(1, 1, 2)).to.eql(expenses); 
+      expect(await split.getExpensesFrom(1, 1)).to.eql(expenses); 
+      expect(await split.getExpensesFrom(1, 2)).to.eql(expenses.slice(1)); 
+      expect(await split.getLastExpenses(1, 1)).to.eql(expenses.slice(1)); 
+      expect(await split.getLastExpenses(1, 2)).to.eql(expenses); 
+    })
+
+    it('should throw "The lists of debtors and debts must be of the same length"', async function () {
+      const { members1, debtors2, debts1, info1 } = await data()
+      await split.createGroup(members1, info1)
+      await expect(split.addExpense(1, debtors2, debts1, info1)).to.be.revertedWith(
+        "The lists of debtors and debts must be of the same length"
+      )
+    })
+
+    it('should throw "You must record at least 1 debt"', async function () {
+      const { members1, info1 } = await data()
+      await split.createGroup(members1, info1)
+      await expect(split.addExpense(1, [], [], info1)).to.be.revertedWith(
+        "You must record at least 1 debt"
+      )
+    })
+
+    it('should throw "You can\'t be a debtor in one of your expenses"', async function () {
+      const { members1, debtors2, debts2, info1 } = await data()
+      await split.createGroup(members1, info1)
+      await expect(split.addExpense(1, debtors2, debts2, info1)).to.be.revertedWith(
+        "You can\'t be a debtor in one of your expenses"
+      )
+    })
+
+    it('should throw "All the debtors must be members of the group"', async function () {
+      const { members1, debtors3, debts1, info1 } = await data()
+      await split.createGroup(members1, info1)
+      await expect(split.addExpense(1, debtors3, debts1, info1)).to.be.revertedWith(
+        "All the debtors must be members of the group"
+      )
+    })
+
+    it('should throw "The amount of the debts must be greater than 0"', async function () {
+      const { members1, debtors1, info1 } = await data()
+      await split.createGroup(members1, info1)
+      await expect(split.addExpense(1, debtors1, [7, 0], info1)).to.be.revertedWith(
+        "The amount of the debts must be greater than 0"
+      )
+    })
   })
 })
